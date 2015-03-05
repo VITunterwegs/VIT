179c2727040c7f69b688f5d66061bb948203fea0
SveKn <sven.kuendahl@gmail.com>
2015-03-05 09:46:00 +0100

    unbedeutende Veränderungen

--- VIT_app/app/src/main/java/com/vitunterwegs/vit_app/MainActivity.java
+++ VIT_app/app/src/main/java/com/vitunterwegs/vit_app/MainActivity.java
@@ -16,38 +16,19 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Anzeige Bereich für die Website
         WebView webView = (WebView)
                 findViewById(R.id.webview);
 
+        // Javascript ermöglichen
         webView.getSettings().setJavaScriptEnabled(true);
 
+        // WebViewClient, damit die Navigation in der WebView bleibt
         WebViewClientImpl webViewClient = new WebViewClientImpl(this);
         webView.setWebViewClient(webViewClient);
 
+        // Anzeige der Website
         webView.loadUrl("http://mathias-foehrenbacher.de/VIT/index.html");
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
 
