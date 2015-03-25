a823a9ba2e7cf06b9d672285ff9b6dda793055e3
Fschoeneborn <fabian@schoeneborn-online.de>
2015-03-25 18:08:30 +0100

    ....

--- Website-SAPUI5/VIT/WebContent/vit/Shell.controller.js
+++ Website-SAPUI5/VIT/WebContent/vit/Shell.controller.js
@@ -1,8 +1,21 @@
 sap.ui.controller("vit.Shell", {
 	onInit : function() {
 		sap.ui.getCore().byId("vMain"+"--"+"splitApp").attachBrowserEvent("swiperight", function(oEvent){
-			var oHashChanger = new sap.ui.core.routing.HashChanger();
+			var oHashChanger = sap.ui.core.routing.HashChanger.getInstance();
 			oHashChanger.setHash(sap.ui.core.routing.Router.getRouter("appRouter").getURL("Navigation"));
+		});
+		
+		sap.ui.getCore().byId("vMain"+"--"+"splitApp").attachBrowserEvent("swipeleft", function(oEvent){
+			var oHistory = sap.ui.core.routing.History.getInstance();
+			var sPreviousHash = oHistory.getPreviousHash();
+
+			// The history contains a previous entry
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				var bReplace = true; // otherwise we go backwards with a forward history
+				this.navTo(sRoute, mData, bReplace);
+			}
 		})
 	},	
 
