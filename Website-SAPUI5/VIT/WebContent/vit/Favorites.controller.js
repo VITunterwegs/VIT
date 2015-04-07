920539a8337e93b79186134ce7b0e0fb3229a547
Fschoeneborn <fabian@schoeneborn-online.de>
2015-04-07 23:04:00 +0200

    .

--- Website-SAPUI5/VIT/WebContent/vit/Favorites.controller.js
+++ Website-SAPUI5/VIT/WebContent/vit/Favorites.controller.js
@@ -3,12 +3,28 @@ jQuery.sap.require("sap.m.MessageToast");
 sap.ui.controller("vit.Favorites", {
 	onInit : function() {
 
+		// Load Favorites
+
+		oModel = new sap.ui.model.json.JSONModel();
+		oModel.loadData("json/favorites.json");
+
+		var oList = this.byId("List");
+
+		oList.setModel(oModel);
+
+		var oItemTemplate = new sap.ui.core.ListItem({
+			text : "Linie {line} - {direction}"
+		});
+		
+			
+		oList.bindItems("/favoriten", oItemTemplate);
+
 	},
-	
+
 	showMenu : function(oEvent) {
 		var oHashChanger = new sap.ui.core.routing.HashChanger();
-		oHashChanger.setHash(sap.ui.core.routing.Router.getRouter("appRouter").getURL("Navigation"));
+		oHashChanger.setHash(sap.ui.core.routing.Router.getRouter("appRouter")
+				.getURL("Navigation"));
 	}
-
 
 });
