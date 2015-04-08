3b8006d0f3b6180c519df22a2b0e306a5613d5b1
SveKn <sven.kuendahl@gmail.com>
2015-04-08 17:55:40 +0200

    ..

--- Website-SAPUI5/VIT/WebContent/vit/Result.controller.js
+++ Website-SAPUI5/VIT/WebContent/vit/Result.controller.js
@@ -4,23 +4,37 @@ sap.ui.controller("vit.Result", {
 	onInit : function() {
 
 	},
-	
+
 	showMenu : function(oEvent) {
 		var oHashChanger = new sap.ui.core.routing.HashChanger();
-		oHashChanger.setHash(sap.ui.core.routing.Router.getRouter("appRouter").getURL("Navigation"));
+		oHashChanger.setHash(sap.ui.core.routing.Router.getRouter("appRouter")
+				.getURL("Navigation"));
 	},
-	handleLiveChange : function (oEvent){
+	handleLiveChange : function(oEvent) {
 		var label = this.byId("rangeLabel");
-		label.setText(oEvent.getSource().getValue()*0.1+" km Umkreis");
+		label.setText(oEvent.getSource().getValue() * 0.1 + " km Umkreis");
 	},
-	handleMapPress : function (oEvent){
+	handleMapPress : function(oEvent) {
 		var source = oEvent.getSource();
-		if (source.getText() == "Kartenansicht" ){
+		if (this.map == null) {
+			this.map = new sap.m.Image({
+				src : "img/dummy.png",
+				width : "100%",
+				height : "100%"
+			});
+		}
+
+		if (source.getText() == "Kartenansicht") {
+			this.liste = this.byId("contentBox").getItems();
+			this.byId("contentBox").removeAllItems();
 			source.setText("Listenansicht");
 			source.setIcon("sap-icon://menu2");
+			this.byId("contentBox").addItem(this.map);
 		} else {
+			this.byId("contentBox").removeAllItems();
 			source.setText("Kartenansicht");
 			source.setIcon("sap-icon://map-2");
+			this.byId("contentBox").addItem(this.liste[0]);
 		}
 	}
 
