f3964bbd4eb3c7018b7a51218daa669afc7c5c84
SveKn <sven.kuendahl@gmail.com>
2015-04-08 17:22:48 +0200

    save_commit

--- Website-SAPUI5/VIT/WebContent/vit/Result.controller.js
+++ Website-SAPUI5/VIT/WebContent/vit/Result.controller.js
@@ -8,7 +8,20 @@ sap.ui.controller("vit.Result", {
 	showMenu : function(oEvent) {
 		var oHashChanger = new sap.ui.core.routing.HashChanger();
 		oHashChanger.setHash(sap.ui.core.routing.Router.getRouter("appRouter").getURL("Navigation"));
+	},
+	handleLiveChange : function (oEvent){
+		var label = this.byId("rangeLabel");
+		label.setText(oEvent.getSource().getValue()*0.1+" km Umkreis");
+	},
+	handleMapPress : function (oEvent){
+		var source = oEvent.getSource();
+		if (source.getText() == "Kartenansicht" ){
+			source.setText("Listenansicht");
+			source.setIcon("sap-icon://menu2");
+		} else {
+			source.setText("Kartenansicht");
+			source.setIcon("sap-icon://map-2");
+		}
 	}
-
 
 });
